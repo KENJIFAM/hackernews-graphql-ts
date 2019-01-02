31228ce2ca068cb3b9bc8d837ec4214eb6dc8ee6
Khoa Pham <kenjifam@gmail.com>
2019-01-02 14:51:20 +0200

    resolvers

--- src/server.ts
+++ src/server.ts
@@ -6,22 +6,47 @@ interface Link {
   url: string;
 }
 
-const links = [{
+let links = [{
   id: 'link-0',
   url: 'www.howtographql.com',
   description: 'Fullstack tutorial for GraphQL'
 }];
 
+let idCount = links.length;
+
 const resolvers = {
   Query: {
-    info: (): unknown => `undefined`,
-    feed: () => links
+    info: (): string => `undefined`,
+    feed: (): Link[] => links,
+    link: (parent: unknown, { id }: Link): Link => links.find(link => link.id === id)
   },
 
-  Link: {
-    id: (parent: Link) => parent.id,
-    description: (parent: Link) => parent.description,
-    url: (parent: Link) => parent.url
+  Mutation: {
+    post: (parent: unknown, { description, url }: Link) => {
+      const link = {
+        id: `link-${idCount++}`,
+        description: description,
+        url: url
+      };
+      links.push(link);
+      return link;
+    },
+    updateLink: (parent: unknown, { id, description, url }: Link) => {
+      const link = { id, description, url };
+      links = links.map(l => l.id === link.id ? link : l);
+      return link;
+    },
+    deleteLink: (parent: unknown, { id }: Link) => {
+      let link: Link = undefined;
+      links = links.filter(l => {
+        if (l.id == id) {
+          link = l;
+          return false;
+        }
+        return true;
+      });
+      return link;
+    }
   }
 };
 
