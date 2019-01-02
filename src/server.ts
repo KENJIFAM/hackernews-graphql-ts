7a13a9550ed430acceb01ad8b08acbcc27eab656
Khoa Pham <kenjifam@gmail.com>
2019-01-02 11:30:22 +0200

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,40 @@
 import { GraphQLServer } from 'graphql-yoga';
 
+interface Link {
+  id: string;
+  description: string;
+  url: string;
+}
+
 const typeDefs = `
 type Query {
   info: String!
+  feed: [Link!]!
+}
+
+type Link {
+  id: ID!
+  description: String!
+  url: String!
 }
 `;
 
+const links = [{
+  id: 'link-0',
+  url: 'www.howtographql.com',
+  description: 'Fullstack tutorial for GraphQL'
+}];
+
 const resolvers = {
   Query: {
-    info: (): unknown => `undefined`
+    info: (): unknown => `undefined`,
+    feed: () => links
+  },
+
+  Link: {
+    id: (parent: Link) => parent.id,
+    description: (parent: Link) => parent.description,
+    url: (parent: Link) => parent.url
   }
 };
 
