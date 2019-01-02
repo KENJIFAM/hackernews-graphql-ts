e5198cb24a3bae3d21ac6f27e6ed2c306ead80d5
Khoa Pham <kenjifam@gmail.com>
2019-01-02 17:39:58 +0200

    prisma binding context

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,6 @@
 import { GraphQLServer } from 'graphql-yoga';
+import { prisma, Prisma } from './generated/prisma-client';
+import { GraphQLResolveInfo } from 'graphql';
 
 interface Link {
   id: string;
@@ -6,53 +8,43 @@ interface Link {
   url: string;
 }
 
-let links = [{
-  id: 'link-0',
-  url: 'www.howtographql.com',
-  description: 'Fullstack tutorial for GraphQL'
-}];
-
-let idCount = links.length;
+export interface Context {
+  prisma: Prisma;
+  request: any;
+}
 
 const resolvers = {
   Query: {
-    info: (): string => `undefined`,
-    feed: (): Link[] => links,
-    link: (parent: unknown, { id }: Link): Link => links.find(link => link.id === id)
+    info: (): string => `This is the API of a Hackernews Clone`,
+    feed: (root: unknown, args: Link, context: Context, info: GraphQLResolveInfo) => context.prisma.links()
+    // link: (parent: unknown, { id }: Link): Link => links.find(link => link.id === id)
   },
 
   Mutation: {
-    post: (parent: unknown, { description, url }: Link) => {
-      const link = {
-        id: `link-${idCount++}`,
-        description: description,
-        url: url
-      };
-      links.push(link);
-      return link;
-    },
-    updateLink: (parent: unknown, { id, description, url }: Link) => {
-      const link = { id, description, url };
-      links = links.map(l => l.id === link.id ? link : l);
-      return link;
-    },
-    deleteLink: (parent: unknown, { id }: Link) => {
-      let link: Link = undefined;
-      links = links.filter(l => {
-        if (l.id == id) {
-          link = l;
-          return false;
-        }
-        return true;
-      });
-      return link;
-    }
+    post: (root: unknown, { description, url }: Link, context: Context) => context.prisma.createLink({ url, description })
+    // updateLink: (parent: unknown, { id, description, url }: Link) => {
+    //   const link = { id, description, url };
+    //   links = links.map(l => l.id === link.id ? link : l);
+    //   return link;
+    // },
+    // deleteLink: (parent: unknown, { id }: Link) => {
+    //   let link: Link = undefined;
+    //   links = links.filter(l => {
+    //     if (l.id == id) {
+    //       link = l;
+    //       return false;
+    //     }
+    //     return true;
+    //   });
+    //   return link;
+    // }
   }
 };
 
 const server = new GraphQLServer({
   typeDefs: './src/schema.graphql',
-  resolvers
+  resolvers,
+  context: { prisma }
 });
 
 server.start(() => console.log('Server is running on port 4000'));
