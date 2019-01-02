070ce5517fdb5d173c8f78b05227b2edd63eadd8
Khoa Pham <kenjifam@gmail.com>
2019-01-03 01:25:42 +0200

    filter, pagination, order links fetching

--- src/resolvers/Query.ts
+++ src/resolvers/Query.ts
@@ -1,9 +1,23 @@
-import { Link } from '../generated/prisma-client';
+import { Link, LinkOrderByInput } from '../generated/prisma-client';
 import { Context } from '../utils';
 import { GraphQLResolveInfo } from 'graphql';
 
+interface Args {
+  filter: string;
+  skip: number;
+  first: number;
+  orderBy: LinkOrderByInput;
+}
+
 export const Query = {
-  feed: async (root: unknown, args: Link, context: Context, info: GraphQLResolveInfo) => {
-    return context.prisma.links();
+  feed: async (root: unknown, { filter, skip, first, orderBy }: Args, context: Context, info: GraphQLResolveInfo): Promise<Link[]> => {
+    const where = filter ? {
+      OR: [
+        { description_contains: filter },
+        { url_contains: filter }
+      ]
+    } : {};
+    const links = await context.prisma.links({ where, skip, first, orderBy });
+    return links;
   }
 };
