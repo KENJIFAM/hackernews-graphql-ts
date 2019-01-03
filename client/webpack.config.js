9c27c6ed8c636c0ede50f26f9f83c59336e08fa0
Khoa Pham <kenjifam@gmail.com>
2019-01-03 20:19:49 +0200

    client init

---
+++ client/webpack.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  entry: './src/index.tsx',
+  output: {
+    filename: './bundle.js'
+  },
+  devtool:'source-map',
+  resolve: {
+    extensions: ['*','.ts','.tsx','.js']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: 'ts-loader'
+      }
+    ]
+  },
+  externals: {
+    "react": "React",
+    "react-dom": "ReactDOM"
+  }
+}
