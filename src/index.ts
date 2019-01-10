5cbb3b8b743326be51b72c7e430c99900a71833c
Khoa Pham <kenjifam@gmail.com>
2019-01-10 02:37:00 +0200

    deploy

--- src/index.ts
+++ src/index.ts
@@ -3,13 +3,28 @@ import { GraphQLServer } from 'graphql-yoga';
 import { prisma } from './generated/prisma-client';
 import resolvers from './resolvers';
 import { typeDefs } from './schema';
+import path from 'path';
+import express, { Request, Response, NextFunction } from 'express';
 
 dotenv.config();
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, '../public')));
+
+const handler = app.get('*', (req: Request, res: Response) => {
+  res.sendFile(path.join(__dirname , '../public/index.html'));
+});
 
 const server = new GraphQLServer({
   typeDefs,
   resolvers,
   context: request => ({ ...request, prisma })
+});
+
+server.use((req: Request, res: Response, next: NextFunction) => {
+  if (req.path.startsWith('/playground') || req.path.startsWith('/graphql')) return next();
+  handler(req, res, next);
 });
 
 server.start({
